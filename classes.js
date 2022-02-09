490d6cda10f1ec900f5d6dec066719bf880ce274
Mark Lawson <mark.lawson@cleveranchor.com>
2022-02-08 20:37:14 -0500

    Updated classes.js with more classes for assignments and researchpaper.

--- classes.js
+++ classes.js
@@ -20,6 +20,10 @@ class Person {
     introduceSelf() {
       console.log(`Hi! I'm ${this.name} and I am ${this.age} years old.`);
     }
+
+    sayHello() {
+        console.log("Hello Everyone!");
+    }
   
   }
 
@@ -30,21 +34,22 @@ class Person {
   
     //to create a new Professor instance
     //name and teaches must be passed to the constructor
-    constructor(name, teaches) {
-      super(name);
+    constructor(name, teaches, age) {
+      super(name, age);
       this.teaches = teaches;
     }
   
     //this overrides the Person class' 
     //introduceSelf() 
     introduceSelf() {
-      console.log(`My name is ${this.name}, and I will be your ${this.teaches} professor.`);
+      console.log(`My name is ${this.name}, and I will be your ${this.teaches} professor. My age is ${this.age}`);
     }
   
     //this method is added for Professor
-    grade(paper) {
+    grade() {
       const grade = Math.floor(Math.random() * (5 - 1) + 1);
       console.log(grade);
+      return grade;
     }
   
   }
@@ -56,6 +61,7 @@ class Person {
     //but if code outside of the object attempts to
     //access #year the browser throws an error
     #year;
+    courses =[];
   
     constructor(name, age, year) {
       super(name, age);
@@ -71,10 +77,15 @@ class Person {
       return this.#year > 1;
     }
 
+    addACourse(aCourse) {
+        this.courses.push(aCourse);
+    }
+
     //the public method promote 
     promote() {
         //when a student is promoted they advance a year.
         this.#advanceAYear();
+        this.courses = [];
     }
 
     //the private method #advanceAYear
@@ -87,16 +98,61 @@ class Person {
   
   }
 
+  class Assignment {
+      title;
+      grade;
 
+      constructor(title, grade) {
+          this.title = title;
+          this.grade = grade;
+      }
+  }
+
+  class ResearchPaper extends Assignment {
+      length;
+      subject;
+      dueDate;
+
+      constructor(title, grade, length, subject, dueDate) {
+        super(title, grade);
+        this.length = length;
+        this.subject = subject;
+        this.dueDate = dueDate;
+      }
+
+      showPaper() {
+          console.log(`My paper ${this.title} is about ${this.subject} it is(was) due ${this.dueDate}. I received a ${this.grade}`);
+      }
+
+      setGrade(grade) {
+          this.grade = grade;
+      }
+
+  }
+
+/*
 const myArray = new Array(1,2);
 myArray.push(9);
 
 const ava = new Person("Ava");
 ava.introduceSelf();
+ava.sayHello();
 
-const naomi = new Professor("Naomi", "History");
-naomi.introduceSelf();
+
+
+
 
 const kim = new Student("Kim", 17, 11);
 kim.introduceSelf();
+kim.promote();
+kim.#advanceAYear();
+*/
+
+const naomi = new Professor("Naomi", "History");
+naomi.introduceSelf();
+naomi.sayHello();
+
+const historypaper = new ResearchPaper("My Title", 0, 10, "Art History", Date.now());
+historypaper.setGrade(naomi.grade());
+historypaper.showPaper();
 
